7a53950d81f2dd8cdad5ef050baf6cdf6d595f63
Shaw <shshaw@users.noreply.github.com>
2021-08-09 07:57:43 -0500

    defaultOptions for ssr

--- data/apollo.js
+++ data/apollo.js
@@ -14,6 +14,25 @@ export function getApolloClient() {
       ssrMode: isServer,
       uri: "https://api.graphql.jobs/",
       cache: new InMemoryCache().restore(windowApolloState || {})
+//      /** Default options to disable SSR for all queries. */
+//       defaultOptions: {
+//         /**
+//          * Skip queries when server side rendering
+//          * https://www.apollographql.com/docs/react/data/queries/#ssr
+//          */
+//         watchQuery: {
+//           ssr: false
+//         },
+//         query: {
+//           ssr: false
+//         }
+//         /**
+//          *Selectively enable specific queries like so:
+//          * ```
+//          * const response = useQuery(QUERY, { ssr: true });
+//          * ```
+//          */
+//      }
     });
   }
 
